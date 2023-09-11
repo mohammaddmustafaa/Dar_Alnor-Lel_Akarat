d4659d5c9983ad728b97ea3d358cc7be340dde13
Mohammad Mustafa <112473854+mohammaddmustafaa@users.noreply.github.com>
2023-09-11 18:02:17 +0000

    Test the new Keyboard and new way of coding on my ipad with githubDiskSpace

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,5 @@
 const heroList = ["Home", "Service", "Property", "About", "Contact"];
-
+const keyboardIpad = ['Test', 'NewKybboard', 'Github-DeskSpace'];
 const listElement = () =>
   heroList.map((element) => (
     <li className="hero-list-element" key={element}>
@@ -10,7 +10,10 @@ const listElement = () =>
 function HeroList() {
   return (
     <>
-      <ul className="heroListContainer">{listElement()}</ul>
+      <ul className="heroListContainer">
+        {listElement()}
+        {keyboardIpad}
+        </ul>
     </>
   );
 }
