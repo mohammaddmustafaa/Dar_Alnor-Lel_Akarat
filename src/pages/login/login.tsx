cffa05b039a9c6c292491071018ca10234890cd8
Mohammad Mustafa <112473854+mohammaddmustafaa@users.noreply.github.com>
2023-09-21 17:34:06 +0300

    create Login Page . inputs and labels , icon bottom right . (background[body]) need to make anther way to do it

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,18 +1,37 @@
 const login = { Name: "Name", Email: "Email", "Phone Number": "Phone Number" };
+const inputType = ["text", "email", "number"];
+const placeholder = ["name", "........@email.com", "*****"];
+const buttons = ["Create account", "Continue with Google"];
 
 const title = () =>
-  Object.keys(login).map(
-    (value, key) => (
-      <form className="login-container">
-        <label className="login-label">{value}</label>
-        <input key={key} className="login-input"></input>
-      </form>
-    ),
-    console.log("wtf")
-  );
+  Object.keys(login).map((value, index) => (
+    <div className="label-container">
+      <label className="login-label">{value}</label>
+      <input
+        placeholder={placeholder[index]}
+        key={value}
+        className="login-input"
+        type={inputType[index]}
+      />
+    </div>
+  ));
+const btn = () =>
+  buttons.map((btn) => <button className="account-login-btns">{btn}</button>);
 
 function Login() {
-  return <div className="login-div-container">{title()}</div>;
+  return (
+    <div className="login-div-container">
+      <form className="login-container">
+        {title()}
+        {btn()}
+      </form>
+      {/* //! backGround Image right Bottom // ??????????????????? */}
+      {/* <div className="cmpny-icon-container"> */}
+      {/* <img className="cmpny-icon" src="src/assets/Sans-titre.png" /> */}
+      {/* </div> */}
+      {/* // ??????????????????? */}
+    </div>
+  );
 }
 
 export default Login;
